e11ad71b9f82e0e7b93640d6f68f1bc6933aecfe
felipeluis223 <lf9072962@gmail.com>
2025-04-02 23:45:59 -0300

    feat: aplicando verificação de token na rota - home.

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function HomeTemplate(){
+    
+    const navigate = useNavigate();
+    
+    // Verificando se há token no localStorage para permitir o acesso:
+    useEffect(() => {
+        const getToken = () => {
+            const token = localStorage.getItem("tokenBeta");
+            if (!token) {
+                navigate("/");
+            };
+        };
+
+    getToken();
+    }, [navigate]);
+    
     return (
         <section>
             <h2>Welcome in Home - test</h2>
