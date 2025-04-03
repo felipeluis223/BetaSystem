2b341249da7c262a464358a7dede08726632ccfd
felipeluis223 <lf9072962@gmail.com>
2025-04-02 23:35:03 -0300

    feat: criação da rota - home.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import HomeTemplate from './templates/Home';
 import LoginTemplate from './templates/Login'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <LoginTemplate />
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={ <LoginTemplate /> } />
+        <Route path="/home" element= { <HomeTemplate/> } />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,9 @@
+function HomeTemplate(){
+    return (
+        <section>
+            <h2>Welcome in Home - test</h2>
+        </section>
+    );
+};
+
+export default HomeTemplate;
