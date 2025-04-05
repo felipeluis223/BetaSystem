5e8fabdf1154bcaf54a9455c19516b6576fefca8
felipeluis223 <lf9072962@gmail.com>
2025-04-04 22:29:24 -0300

    refactor: nome e icone no header.

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,15 +1,17 @@
 import menuItems from "./data";
+import { IoPersonCircleOutline } from "react-icons/io5";
 
 export default function HomeHeader() {
     const nameMock: string = "Luis Felipe";
 
     return (
         <header className="w-full h-auto bg-[#040404] flex flex-row justify-between shadow-md relative z-50">
-            <section className="w-[20%] h-[80px] flex items-center justify-center">
-                <h3 className="font-bold text-white text-lg">Olá {nameMock}!</h3>
+            <section className="w-[20%] h-[80px] flex items-center justify-center gap-[10px] font-bold text-white">
+                <IoPersonCircleOutline className="text-[2rem] text-[#ffffff]" />
+                <h3 className="text-lg text-[#22c55e]">{nameMock}</h3>
             </section>
 
-            <nav className="w-[60%] h-[80px] flex items-center">
+            <nav className="w-[65%] h-[80px] flex items-center">
                 <ul className="flex gap-6 text-white font-medium px-4 relative z-50">
                     {menuItems.map((menu) => (
                         <li key={menu.title} className="relative group">
