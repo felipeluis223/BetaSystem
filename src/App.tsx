1944dab3d2098e6efc7b8b39211db5d366d8c00b
felipeluis223 <lf9072962@gmail.com>
2025-04-09 23:50:04 -0300

    refactor: removendo mock.

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,7 @@ import LoginTemplate from './templates/Login'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  // Mock de dados simulando o que viria da API
-  // const employeeData = {
-  //   title: "Tabela de Funcionários",
-  //   table: {
-  //     data: [
-  //       { column: "id", data: "7896321" },
-  //       { column: "name", data: "Luis Felipe" },
-  //       { column: "cpf", data: "23634697898" },
-  //       { column: "rg", data: "794398234" },
-  //       { column: "email", data: "luis@gmail.com" },
-  //       { column: "phone", data: "23644697865" },
-  //     ],
-  //   },
-  // };
-
+  
   return (
     <Router>
       <Routes>
