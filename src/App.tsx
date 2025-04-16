dba6c779c966890b772f1b5860d813accb0ce629
felipeluis223 <lf9072962@gmail.com>
2025-04-15 21:28:30 -0300

    fix: obter token no template - Login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContentManager from './components/ContentManager';
 import NotFoundPage from './components/NotFoundPage';
 import HomeTemplate from './templates/Home';
 import LoginTemplate from './templates/Login';
-import { useSelector } from 'react-redux';
-import { RootState } from './redux/store';
-
-function RedirectOnToken() {
-  const navigate = useNavigate();
-  const token = useSelector((state: RootState) => state.auth.token);  // Acessa o token do Redux
-
-  useEffect(() => {
-    // Verifica se o token existe e se a rota atual não é '/home'
-    if (token && window.location.pathname !== "/home") {
-      navigate("/home");
-    }
-  }, [token, navigate]);  // O useEffect só será chamado quando o token mudar
-
-  return null;  // Não precisa renderizar nada
-}
 
 function App() {
   return (
     <Router>
-      <RedirectOnToken /> {/*} Componente de redirecionamento */}
       <Routes>
         <Route path="/" element={<LoginTemplate />} />
         <Route path="/home" element={<HomeTemplate />}>
--- src/templates/Login/index.tsx
+++ src/templates/Login/index.tsx
@@ -1,7 +1,23 @@
 import LoginForm from "../../components/LoginForm";
 import LoginWallpaper from "../../components/LoginWallpaper";
 
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 export default function LoginTemplate (){
+    const navigate = useNavigate();
+    const token = useSelector((state: RootState) => state.auth.token);  // Acessa o token do Redux
+  
+    useEffect(() => {
+      // Verifica se o token existe e se a rota atual não é '/home'
+      if (token && window.location.pathname !== "/home") {
+        navigate("/home");
+      }
+    }, [token, navigate]);  // O useEffect só será chamado quando o token mudar
+
+    
     return (
         <section className="w-full h-screen bg-[#f1f1f1] flex flex-row">
             <LoginWallpaper />
