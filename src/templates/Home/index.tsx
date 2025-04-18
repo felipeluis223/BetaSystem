97b78b71b09e396a7849056508c248ae125a6152
felipeluis223 <lf9072962@gmail.com>
2025-04-18 12:01:53 -0300

    feat: aplicando validação de token.

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,17 +1,41 @@
 import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import HomeHeader from "../../components/HomeHeader";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../redux/store";
+import { jwtDecode } from "jwt-decode";
+import { clearToken } from "../../redux/authSlice";
 
 function HomeTemplate() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const dispatch = useDispatch();
     const token = useSelector((state: RootState) => state.auth.token);
+
     useEffect(() => {
+        const redirectToLogin = () => {
+            dispatch(clearToken()); // Limpa o token do Redux
+            if (location.pathname !== "/") {
+                navigate("/");
+            }
+        };
+
         if (!token) {
-            navigate("/");
+            redirectToLogin();
+        } else {
+            try {
+                const decodedToken: any = jwtDecode(token);
+                const currentTime = Math.floor(Date.now() / 1000);
+
+                if (decodedToken.exp && decodedToken.exp < currentTime) {
+                    redirectToLogin();
+                }
+            } catch (error) {
+                console.error("Erro na decodificação do token", error);
+                redirectToLogin();
+            }
         }
-    }, [token, navigate]);
+    }, [token, navigate, location, dispatch]);
 
     return (
         <section className="w-full h-screen">
