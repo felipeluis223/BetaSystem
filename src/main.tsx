3fbddb7e1980077eef2b7831ae7dce8425e55817
felipeluis223 <lf9072962@gmail.com>
2025-04-19 09:53:42 -0300

    feat: configurando o login com a google.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-import { Provider } from 'react-redux'
-import store, { persistor } from './redux/store.ts'
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  // Lembrando que o clientID é obtido na plataforma da google: https://console.cloud.google.com/
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_ID_CLIENT}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </GoogleOAuthProvider>
 )
