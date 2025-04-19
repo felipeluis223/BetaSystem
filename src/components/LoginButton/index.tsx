9a74847e48a841f0da037aa05299acd6ec56db30
felipeluis223 <lf9072962@gmail.com>
2025-04-19 10:46:28 -0300

    chore: aplicando o login Google na opção criada.

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,5 +1,9 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaApple } from "react-icons/fa";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setToken } from "../../redux/authSlice";
 
 // Botão de "Esqueci a senha":
 export function ForgotPassword(){
@@ -17,10 +21,25 @@ export function LoginButton(){
 
 // Botões de outras maneiras de realizar o login (Google, Facebook e Apple):
 export function OptionsButton(){
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const loginWithGoogle = useGoogleLogin({
+        onSuccess: (tokenResponse) => {
+          const { access_token } = tokenResponse;
+          dispatch(setToken(access_token));
+          navigate("/home");
+        },
+        onError: () => {
+          console.log("Erro ao fazer login com o Google");
+        },
+        flow: 'implicit',
+        ux_mode: 'redirect'
+      });
+
     return (
         <div className="w-full h-[50px]">
             <ul className="text-[2rem] flex items-center justify-center w-full h-full gap-[20px]">
-                <li><button type="button" className="cursor-pointer"><FcGoogle /></button></li>
+                <li><button type="button" className="cursor-pointer" onClick={loginWithGoogle}><FcGoogle /></button></li>
                 <li className="text-[#1877f2]"><button type="button" className="cursor-pointer"><FaFacebook /></button></li>
                 <li><button type="button" className="cursor-pointer"><FaApple className="text-[#000000]" /></button></li>
             </ul>
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,67 +1,61 @@
 import { useState } from "react";
-import {TitleLogin, SubTitleLogin} from "../LoginTitle";
+import { TitleLogin, SubTitleLogin } from "../LoginTitle";
 import { LoginButton, OptionsButton, ForgotPassword } from "../LoginButton";
 import { EmailInput } from "../LoginEntryEmail";
 import { PasswordInput } from "../LoginInputPassword";
-
 import { RegisterAccountButton } from "../RegisterAccountButton";
 import RegisterUserModal from "../RegisterAccountForm";
+
 import loginAPI from "./login";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setToken } from "../../redux/authSlice";
 
+// Google Login
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
+
 type UserData = {
-    email: string,
-    password: string
+    email: string;
+    password: string;
 };
 
-export default function LoginForm(){
-    const [ showPassword, setShowPassword ] = useState<boolean>(false);
-    const [ userData, setUserData ] = useState<UserData>({email: "", password: ""});
-    const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false);
+export default function LoginForm() {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [userData, setUserData] = useState<UserData>({ email: "", password: "" });
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    // Função responsável pelo botão de exibir ou ocultar senha:
     const togglePasswordVisibility = () => setShowPassword(prev => !prev);
 
-    // Obtendo e atualizando os dados fornecidos do campo de email e senha:
-    const handleChange = (field: string, value: string)=>{
-        setUserData((prevData)=>({
+    const handleChange = (field: string, value: string) => {
+        setUserData((prevData) => ({
             ...prevData,
             [field]: value,
         }));
     };
 
-    // Função responsável por realizar a verificação e autenticação do login:
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-
-        // Impedir o comportamento padrão do evento:
         event.preventDefault();
 
-        try{
-            const userToken = await loginAPI({"email": userData.email, "password": userData.password });
+        try {
+            const userToken = await loginAPI({
+                email: userData.email,
+                password: userData.password
+            });
 
-            if(userToken != null){
-                
-                dispatch(setToken(userToken)); // Armazenando o token no estado da aplicação.
+            if (userToken != null) {
+                dispatch(setToken(userToken));
 
-                // Zerando os valores - input do login:
-                setUserData({
-                    email: "",
-                    password: ""
-                });
+                setUserData({ email: "", password: "" });
 
-                navigate("/home"); // Navegando para a home.
+                navigate("/home");
+            } else {
+                alert("Houve um problema no login. Verifique suas informações e tente novamente.");
             }
-            else{
-                alert("Houve um problema no cadastro. Verifique suas informações e tente novamente.");
-            }
-
-        }
-        catch(e){
+        } catch (e) {
             alert("Ocorreu um erro inesperado no servidor.");
         }
     };
@@ -72,11 +66,20 @@ export default function LoginForm(){
                 <TitleLogin />
                 <SubTitleLogin />
 
-                <div className="w-full md:w-[80%] h-[200px]" >
+                <div className="w-full md:w-[80%] h-[200px]">
                     <form className="w-full h-full flex flex-col gap-[15px]" onSubmit={handleSubmit}>
-                        <EmailInput value={userData.email} onChange={(e)=>handleChange("email", e)} />
-                        <PasswordInput placeholder="********" value={userData.password} onChange={(e)=>handleChange("password", e)} visibility={showPassword} togglePasswordVisibility={togglePasswordVisibility}/>
-                        
+                        <EmailInput
+                            value={userData.email}
+                            onChange={(e) => handleChange("email", e)}
+                        />
+                        <PasswordInput
+                            placeholder="********"
+                            value={userData.password}
+                            onChange={(e) => handleChange("password", e)}
+                            visibility={showPassword}
+                            togglePasswordVisibility={togglePasswordVisibility}
+                        />
+
                         <ForgotPassword />
                         <div className="w-full h-[180px] flex flex-col gap-[25px]">
                             <div className="w-full h-full flex justify-center md:justify-start gap-[10px]">
@@ -85,21 +88,21 @@ export default function LoginForm(){
                         </div>
                     </form>
 
-                    <RegisterAccountButton onClick={() => setIsModalOpen(true)}  />
+                    <RegisterAccountButton onClick={() => setIsModalOpen(true)} />
 
-                    <hr className="text-[#d1d1d1]"/>
+                    <hr className="text-[#d1d1d1]" />
 
-                    <div className="w-full h-[100px] mt-[20px] flex flex-col text-center gap-[15px]">    
+                    <div className="w-full h-[100px] mt-[20px] flex flex-col text-center gap-[15px]">
                         <p className="w-full h-[20px] text-[14px] text-[#808080]">Outras formas de realizar o login:</p>
+
                         <OptionsButton />
                     </div>
-
                 </div>
             </section>
 
             {
-                isModalOpen && <RegisterUserModal onClose={()=>setIsModalOpen(false)} />
+                isModalOpen && <RegisterUserModal onClose={() => setIsModalOpen(false)} />
             }
         </>
     );
-};
+}
