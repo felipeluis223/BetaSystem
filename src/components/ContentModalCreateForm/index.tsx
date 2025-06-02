1777c4ed2cb5c81496c23537e15c90ee39bb1497
felipeluis223 <lf9072962@gmail.com>
2025-06-01 23:36:57 -0300

    chore: mudança no titulo - style.

--- src/components/ContentModalCreateForm/index.tsx
+++ src/components/ContentModalCreateForm/index.tsx
@@ -5,7 +5,7 @@ export default function ContentModalCreateForm(){
         <section className="modal-overlay flex justify-center items-center fixed inset-0 bg-black bg-opacity-40 z-50">
             <div className="modal-content w-full max-w-[600px] bg-[#f1f1f1] p-[20px] rounded-[10px]">
                 <div className="w-full flex justify-between mb-[10px]">
-                <h3 className="text-[1.2rem] font-bold">Formulário de Edição</h3>
+                <h3 className="text-[1.2rem] font-bold">Formulário de Cadastro de Conteúdo</h3>
                 <button className="cursor-pointer">
                     <MdClose className="text-[1.5rem]" />
                 </button>
