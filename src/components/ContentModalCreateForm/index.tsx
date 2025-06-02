d6d650fa4513737ca7a9f00f25801cc229474cc5
felipeluis223 <lf9072962@gmail.com>
2025-06-01 23:34:20 -0300

    chore: aplicando o padrão do modal - style.

--- src/components/ContentModalCreateForm/index.tsx
+++ src/components/ContentModalCreateForm/index.tsx
@@ -1,5 +1,21 @@
+import { MdClose } from "react-icons/md";
+
 export default function ContentModalCreateForm(){
-    return(
-        <div>Formulário</div>
-    );
+    return (
+        <section className="modal-overlay flex justify-center items-center fixed inset-0 bg-black bg-opacity-40 z-50">
+            <div className="modal-content w-full max-w-[600px] bg-[#f1f1f1] p-[20px] rounded-[10px]">
+                <div className="w-full flex justify-between mb-[10px]">
+                <h3 className="text-[1.2rem] font-bold">Formulário de Edição</h3>
+                <button className="cursor-pointer">
+                    <MdClose className="text-[1.5rem]" />
+                </button>
+                </div>
+
+                <span className="text-[#808080] text-[14px]">
+                Edite os dados desejados:
+                </span>
+
+            </div>
+        </section>
+    )
 }
